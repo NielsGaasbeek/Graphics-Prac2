ff16833b2d806cb99b8dc4704e05f3d8a8c1beec
Brian <brianvanbeusekom@hotmail.com>
2017-05-11 15:38:06 +0200

    lel

--- game.cs
+++ game.cs
@@ -3,21 +3,22 @@ using System.IO;
 
 namespace Template {
 
-class Game
-{
-	// member variables
-	public Surface screen;
-	// initialize
-	public void Init()
-	{
-	}
-	// tick: renders one frame
-	public void Tick()
-	{
-		screen.Clear( 0 );
-		screen.Print( "hello world", 2, 2, 0xffffff );
-        screen.Line(2, 20, 160, 20, 0xff0000);
-	}
-}
+    class Game
+    {
+	    // member variables
+	    public Surface screen;
+	    // initialize
+	    public void Init()
+	    {
+	    }
 
-} // namespace Template
+	    // tick: renders one frame
+	    public void Tick()
+	    {
+		    screen.Clear( 0 );
+		    screen.Print( "hello world", 2, 2, 0xffffff );
+            screen.Line(2, 20, 160, 20, 0xff0000);
+	    }
+    }
+
+}
