56c26fa4938bb33845edf36a5b0b0fa7cb73eb96
Brian <brianvanbeusekom@hotmail.com>
2017-05-11 15:54:26 +0200

    struct

--- RayTracer.cs
+++ RayTracer.cs
@@ -1,12 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
-namespace template
+namespace Template
 {
     class RayTracer
     {
+        // member variables
+        public Surface screen;
+        // initialize
+        public void Init()
+        {
+        }
+
+        // tick: renders one frame
+        public void Tick()
+        {
+            screen.Clear(0);
+            screen.Print("hello world", 2, 2, 0xffffff);
+            screen.Line(2, 20, 160, 20, 0xff0000);
+        }
     }
 }
