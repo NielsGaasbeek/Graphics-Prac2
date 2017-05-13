ee6cb0af938245e23b2f457f4995e9495cab9a00
Niels <c.g.gaasbeek@students.uu.nl>
2017-05-13 14:51:00 +0200

    added primitive basics

---
+++ SceneObjects/Primitives/Primitive.cs
@@ -0,0 +1,20 @@
+﻿using OpenTK;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application
+{
+    class Primitive
+    {
+        Vector3 primitiveColor;
+
+        public Primitive(Vector3 color)
+        {
+            primitiveColor = color;
+        }
+    }
+}
---
+++ SceneObjects/Primitives/Sphere.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK;
+using System.Drawing;
+
+namespace Application
+{
+    class Sphere : Primitive
+    {
+        Vector3 centerPos;
+        float radius;
+
+        public Sphere(Vector3 pos, float r, Vector3 color) : base(color)
+        {
+            centerPos = pos;
+            radius = r;
+        }
+
+    }
+}
