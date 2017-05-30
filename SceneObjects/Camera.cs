2b9409635308031879e3e055bf665e5a220fb782
c.g.gaasbeek <c.g.gaasbeek@students.uu.nl>
2017-05-30 18:15:00 +0200

    comments

--- SceneObjects/Camera.cs
+++ SceneObjects/Camera.cs
@@ -37,30 +37,36 @@ namespace Application
             p1 = p1 + new Vector3(0.05f * up, 0.05f * right, 0.05f * away);
             p2 = p2 + new Vector3(0.05f * up, 0.05f * right, 0.05f * away);
         }
+
         public void rotate(float up, float right)
         {
+            //rotating the camera is done by adding or substracting the up- or right-vector based on what rotation is desired
             camera_direction += (right * right_direction);
             camera_direction += (up * up_direction);
 
+            //after that, all the neccesary variables are recalculated
             CalcVars();
         }
 
-
+        //this method calculates all variables needed for the camera to function properly
         public void CalcVars()
         {
+            //a seperate variable for the normalized view-direction is calculated
             Vector3 normalized_direction = Normalize(camera_direction);
 
+            //which is then used to calculate the screen center
             screenCenter = position + d * normalized_direction;
             
+            //up- and right-vectors, used for many of the calculations and translations
             right_direction = Vector3.Cross(normalized_direction, up_direction);
             right_direction = Normalize(right_direction);
             up_direction = Vector3.Cross(right_direction, normalized_direction);
             up_direction = Normalize(up_direction);
 
-            //corners, transform camera by multiplying E,p0,1,2 with camera matrix
-            p0 = screenCenter + -1 * right_direction + 1 * up_direction; //Only if direction (0,0,1)
-            p1 = screenCenter + 1 * right_direction + 1 * up_direction; //Only if direction (0,0,1)
-            p2 = screenCenter + -1 * right_direction + -1 * up_direction; //Only if direction (0,0,1)
+            //corners of the screen-plane are then calculated using the screen center and up- and right-vectors
+            p0 = screenCenter + -1 * right_direction + 1 * up_direction;
+            p1 = screenCenter + 1 * right_direction + 1 * up_direction;
+            p2 = screenCenter + -1 * right_direction + -1 * up_direction;
         }
 
         Vector3 Normalize(Vector3 v)
